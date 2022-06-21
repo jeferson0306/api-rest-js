11af4f00aae253adbd1d8a6292232b2da94365a4
Jeferson Siqueira <jeferson0306@msn.com>
2022-06-21 00:23:00 -0300

    master
    ApiOperation and ApiResponses configuration

--- src/main/java/com/apirestjs/apirestjs/controllers/UserController.java
+++ src/main/java/com/apirestjs/apirestjs/controllers/UserController.java
@@ -3,12 +3,15 @@ package com.apirestjs.apirestjs.controllers;
 import com.apirestjs.apirestjs.models.UserModel;
 import com.apirestjs.apirestjs.services.UserService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -22,23 +25,42 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/fetch/{userId}")
-    @ApiOperation("Returns Uer based in user id .")
-    public UserModel getUserById(@PathVariable  String userId){
+    @ApiOperation("Returns users based in userId")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Indicates success in fetching customer data"),
+            @ApiResponse(code = 404, message = "Indicates failure to fetch customer data")
+    })
+    public UserModel getUserById(@PathVariable  @Valid String userId){
         return userService.getUserById(userId);
     }
 
     @GetMapping("/fetchAll")
+    @ApiOperation("Returns all users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Indicates success in fetching customer data"),
+            @ApiResponse(code = 404, message = "Indicates failure to fetch customer data")
+    })
     public List<UserModel> getAllUsers(){
         return userService.getAllUsers();
     }
 
     @DeleteMapping("/delete/{userid}")
-    public String deleteUserById(@PathVariable  String  userId){
+    @ApiOperation(value = "Delete an user by userId")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Indicates success in delete customer data"),
+            @ApiResponse(code = 404, message = "Indicates failure in delete  customer data")
+    })
+    public String deleteUserById(@PathVariable  @Valid String  userId){
         return userService.deleteUserById(userId);
     }
 
     @PostMapping("/create")
-    public UserModel saveUser(@RequestBody UserModel userModel){
+    @ApiOperation("Create an user and save in database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Indicates successful parameter validation"),
+            @ApiResponse(code = 404, message = "Indicates error in parameter validation")
+    })
+    public UserModel saveUser(@RequestBody @Valid UserModel userModel){
         return userService.saveUser(userModel);
     }
 
